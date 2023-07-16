bc9a3059ab1a04a79a46c9cea43db1c84a2633e1
BohdanStarunskyi <bogdan.starunskiy@gmailcom>
2023-07-16 23:19:10 +0200

    added models

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const login = require('./login/login_module');
 const NotesModule = require('./notes/notes_module')
+const LoginRequeestModel = require('./models/login_models')
+const NotesModel = require('./models/notes_models')
+
 
 const app = express();
 
@@ -13,32 +16,48 @@ app.use(morgan('combined'));
 app.use(express.json());
  
 app.post('/login', (req, res) => {
-  const { email, password } = req.body;
-  login(email, password, res);
+  const model = new LoginRequeestModel(req.body)
+  login(model, res);
 });
 
 app.post('/note', (req, res) => {
   const ownerId = req.headers.ownerid
-  const {title, body} = req.body;
-  NotesModule.saveNote(title, body, ownerId, res);
+  if(ownerId == null || ownerId == "")
+    res.status(422).json({result: "ownerId can't be null or empty"});
+  else{
+    const model = new NotesModel(req.headers.ownerid, req.body.title, req.body.body);
+    NotesModule.saveNote(model, res);
+  }
 });
 
 app.get('/notes', (req, res) => {
-  const ownerId = req.headers.ownerid
-  NotesModule.getAllNotesForUser(ownerId, res);
+  const ownerId = req.headers.ownerid;
+  if(ownerId == null || ownerId == "")
+    res.status(422).json({result: "ownerId can't be null or empty"});
+  else
+    NotesModule.getAllNotesForUser(ownerId, res);
 });
 
 app.delete('/note', (req, res) => {
   const noteId = req.query.noteId;
   const ownerId = req.headers.ownerid;
-  console.log(req.headers);
-  NotesModule.deleteNote(noteId, ownerId, res);
+  if(ownerId == null || ownerId == "")
+    res.status(422).json({result: "ownerId can't be null or empty"});
+  else{
+    const model = new NotesModel(ownerId, null, null, noteId);
+    NotesModule.decleteNote(model, res);
+  }
 });
 
 app.put('/note', (req, res) => {
   const ownerId = req.headers.ownerid
-  const {noteId, title, body} = req.body;
-  NotesModule.updateNote(title, body, ownerId, noteId, res)
+  const noteId = req.body.noteId
+  if(ownerId == null || noteId == null || ownerId == "" || noteId == "")
+    res.status(422).json({result: "ownerId and noteId can't be null or empty"});
+  else{
+  const model = new NotesModel(ownerId, req.body.title, req.body.body, noteId)
+  NotesModule.updateNote(model, res);
+  }
 });
 
 app.listen(3001, () => {
